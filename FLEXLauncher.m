2a731adf6ab4c26892c7b8a8cab526bf67b16bdc
LJSE <116199452+Tw1nk20@users.noreply.github.com>
2026-08-14 12:50:23 +0900

    Auto-launch FLEX in X 12.16 debug build

---
+++ FLEXLauncher.m
@@ -0,0 +1,38 @@
+#import <UIKit/UIKit.h>
+#import <objc/runtime.h>
+
+static void BHTTryShowFLEX(NSUInteger attempt) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        Class flexManagerClass = NSClassFromString(@"FLEXManager");
+        SEL sharedManagerSelector = NSSelectorFromString(@"sharedManager");
+        SEL showExplorerSelector = NSSelectorFromString(@"showExplorer");
+
+        if (flexManagerClass && [flexManagerClass respondsToSelector:sharedManagerSelector]) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+            id manager = [flexManagerClass performSelector:sharedManagerSelector];
+            if (manager && [manager respondsToSelector:showExplorerSelector]) {
+                NSLog(@"[BHTwitter][FLEX] FLEXManager found; showing explorer");
+                [manager performSelector:showExplorerSelector];
+#pragma clang diagnostic pop
+                return;
+            }
+#pragma clang diagnostic pop
+        }
+
+        if (attempt < 10) {
+            NSLog(@"[BHTwitter][FLEX] FLEXManager not ready (attempt %lu)", (unsigned long)attempt);
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                BHTTryShowFLEX(attempt + 1);
+            });
+        } else {
+            NSLog(@"[BHTwitter][FLEX] FLEXManager was not loaded after 10 attempts");
+        }
+    });
+}
+
+__attribute__((constructor)) static void BHTInstallFLEXLauncher(void) {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        BHTTryShowFLEX(1);
+    });
+}
